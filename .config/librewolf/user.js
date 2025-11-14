0a7fe74880888beaeda881a6d12b0b87db2b794f
LazyDev <devbutlazy@gmail.com>
2025-11-14 15:58:57 +0200

    Added LibreWolf settings (can be used for firefox)

---
+++ .config/librewolf/user.js
@@ -0,0 +1,15 @@
+
+// Home Page and New Window
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.startup.page", 0);
+user_pref("browser.startup.homepage", "about:blank");
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.tabs.inTitlebar", 1);
+
+// Disable sponsored content on Firefox Home & Clear default topsites
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.activity-stream.default.sites", "");
+
+// Integrated calculator at urlbar
+user_pref("browser.urlbar.suggest.calculator", true);
